e7fd716dba52da9e79e8971ea2ddd2e81238298e
DanPeled <98838880+DanPeled@users.noreply.github.com>
2024-08-31 12:39:01 +0300

    GitHub Pages Deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1142671476"
+    serviceWorkerVersion: "523693744"
   }
 });
